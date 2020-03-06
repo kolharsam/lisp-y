83376658f4ce9640fe970a54e9b30e34416fcf7e
Sameer Kolhar <kolhar730@gmail.com>
2020-03-06 09:30:39 +0530

    Update evaluate method with some optimisation
    The issue was to bring some optimisation in the way we process the argument list
    within a list. The process more or less remains the same, a recursive reduction. But
    how we do it is changed slightly.

--- eval/index.js
+++ eval/index.js
@@ -5,6 +5,18 @@
 
 const supportedMethods = require("../lib");
 
+function reducedArgList(args) {
+    return args.reduce((currentList, currentElement) => {
+        if (!Array.isArray(currentElement)) {
+            return [...currentList, currentElement];
+        }
+        
+        // NOTE: I don't know if this will improve the performance
+        // but the point being that the recursive action is `tail`-ed
+        return [...currentList, evaluate(currentElement)];
+    }, []);
+}
+
 function evaluate(ast) {
     if (!Array.isArray(ast)) {
         throw new Error("Not a valid AST");
@@ -16,17 +28,12 @@ function evaluate(ast) {
     }
 
     const [func, ...args] = ast;
-
-    let argList = args.reduce((currentList, currentElement) => {
-        if (Array.isArray(currentElement)) {
-            return [...currentList, evaluate(currentElement)];
-        }
-
-        return [...currentList, currentElement];
-    }, []);
-
-    // TODO: Tail call optimisation? Read More.
-    return supportedMethods[func](...argList);
+    
+    if (!supportedMethods[func]) {
+        throw new Error(`${func} method doesn't exist!`);
+    }
+    
+    return supportedMethods[func](...reducedArgList(args));
 }
 
 module.exports = evaluate;
