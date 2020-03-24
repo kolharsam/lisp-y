0cf48e8f55eeae4b872a3a3380c6f2cb0e7010bb
Sameer Kolhar <kolhar730@gmail.com>
2020-03-24 14:36:51 +0530

    Add the reducer for the store
    In the previous commit we added a createVarStore to help us maintain
    an immutable data store for bindings. This commit adds the reducer
    that will needed for the createVarStore.

---
+++ store/reducer.js
@@ -0,0 +1,53 @@
+// This file contains the main reducers that will be used
+// for performing updates on the store.
+
+// The main "dispatch" actions are being fetched from a different file
+
+const globalActions = require("./dispatch").actions;
+
+/**
+ * The main reducer being used for the store
+ * @param {Object} state
+ * @param {string} action
+ * @returns {Object} - The updated state or the current state
+ */
+const storeReducer = (state = {}, action) => {
+    let name, value;
+
+    // Handled for the initial call to dispatch
+    if (!action.data) {
+        return state;
+    }
+
+    // Check if a name is provided
+    if (action.data.name) {
+        name = action.data.name;
+    }
+
+    // Check if a value is provided
+    if (action.data.value) {
+        value = action.data.name;
+    }
+
+    // If only a name is provided, then it is a delete operation
+    // If both a name and value are provided through action.data
+    // then it can be either creation of the variable, or if it
+    // is already present then it is an update operation
+
+    switch (action.type) {
+        case globalActions.GLOBAL_VAR_UPDATE:
+            return { ...state, [name]: value };
+        case globalActions.GLOBAL_VAR_DELETE:
+            return Object.keys(state).reduce((updatedState, currentKey) => {
+                if (currentKey === name) {
+                    return updatedState;
+                }
+
+                return { ...updatedState, [currentKey]: state[currentKey] };
+            }, {});
+        default:
+            return state;
+    }
+};
+
+module.export = storeReducer;
